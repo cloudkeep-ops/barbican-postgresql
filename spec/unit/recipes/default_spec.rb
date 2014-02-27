2d9cb320f53233d086e734778bf23f2e30caf7bf
Steven Gonzales <stevendgonzales@gmail.com>
2014-02-26 19:01:25 -0600

    updated spec tests

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -16,15 +16,48 @@ describe 'barbican-postgresql::default' do
   end
 
   it 'creates barbican_api database' do
-    expect(chef_run).to create_postgresql_database('barbican_api')
+    expect(chef_run).to create_postgresql_database('barbican_api').with(
+      :connection => {
+        :host => 'localhost',
+        :port => chef_run.node['postgresql']['config']['port'],
+        :username => 'postgres',
+        :password => chef_run.node['postgresql']['password']['postgres']
+      },
+      :action => [:create],
+      :retries => chef_run.node['postgresql']['db_actions']['retries'],
+      :retry_delay => chef_run.node['postgresql']['db_actions']['retry_delay']
+    )
   end
 
   it 'creates barbican db user' do
-    expect(chef_run).to create_database_user('barbican')
+    expect(chef_run).to create_database_user('barbican').with(
+      :connection => {
+        :host => 'localhost',
+        :port => chef_run.node['postgresql']['config']['port'],
+        :username => 'postgres',
+        :password => chef_run.node['postgresql']['password']['postgres']
+      },
+      :password => chef_run.node['postgresql']['password']['barbican'],
+      :action => [:create],
+      :retries => chef_run.node['postgresql']['db_actions']['retries'],
+      :retry_delay => chef_run.node['postgresql']['db_actions']['retry_delay']
+    )
   end
 
   it 'grants barbican db user all privelages' do
-    expect(chef_run).to grant_postgresql_database_user('barbican')
+    expect(chef_run).to grant_postgresql_database_user('barbican').with(
+      :connection => {
+        :host => 'localhost',
+        :port => chef_run.node['postgresql']['config']['port'],
+        :username => 'postgres',
+        :password => chef_run.node['postgresql']['password']['postgres']
+      },
+      :database_name => 'barbican_api',
+      :privileges => [:all],
+      :action => [:grant],
+      :retries => chef_run.node['postgresql']['db_actions']['retries'],
+      :retry_delay => chef_run.node['postgresql']['db_actions']['retry_delay']
+    )
   end
 
 end
